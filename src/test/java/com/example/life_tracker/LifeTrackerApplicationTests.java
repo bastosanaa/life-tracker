44771c0c3981697d187648a926fd607414fe645f
Ana Bastos <bastosanaa11@gmail.com>
2026-03-18 09:24:14 -0300

    ci: fix ci pipeline

--- src/test/java/com/example/life_tracker/LifeTrackerApplicationTests.java
+++ src/test/java/com/example/life_tracker/LifeTrackerApplicationTests.java
@@ -1,10 +1,16 @@
 package com.example.life_tracker;
 
+import com.example.life_tracker.IT.IntegrationTestBase;
+import com.example.life_tracker.IT.TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
-@SpringBootTest
-class LifeTrackerApplicationTests {
+@SpringBootTest(properties = {
+		"GOOGLE_API_KEY=fake-key-for-tests"
+})
+@Import(TestConfig.class)
+class LifeTrackerApplicationTests extends IntegrationTestBase {
 
 	@Test
 	void contextLoads() {
